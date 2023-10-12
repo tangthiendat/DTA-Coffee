fe02afbe4f30cd9f4badb3aa18e16e5cb7adc17d
Tang Thien Dat <thiendattang@gmail.com>
2023-10-12 11:54:02 +0700

    Create the splash screen

--- src/main/java/com/ttd/dtacoffee/App.java
+++ src/main/java/com/ttd/dtacoffee/App.java
@@ -3,24 +3,19 @@ package com.ttd.dtacoffee;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
+
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.util.Objects;
 
 public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("/fxml/appView.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/splashScreen.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("DTA Coffee Mangagement System");
-        //Add icon
-        Image icon256 = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/img/official-logo-256.png")));
-        stage.getIcons().add(icon256);
         stage.setScene(scene);
-        stage.setMaximized(true);
+        stage.initStyle(StageStyle.UNDECORATED);
         stage.show();
     }
 
--- src/main/java/com/ttd/dtacoffee/AppLauncher.java
+++
@@ -1,7 +0,0 @@
-package com.ttd.dtacoffee;
-
-public class AppLauncher {
-    public static void main(String[] args) {
-        App.main(args);
-    }
-}
