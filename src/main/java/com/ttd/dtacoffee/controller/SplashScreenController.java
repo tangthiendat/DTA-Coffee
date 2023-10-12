d427aa4e5e5d60c7037f1ec71bc7a76e795de05e
Tang Thien Dat <thiendattang@gmail.com>
2023-10-12 11:56:41 +0700

    Create the splash screen

---
+++ src/main/java/com/ttd/dtacoffee/controller/SplashScreenController.java
@@ -0,0 +1,61 @@
+package com.ttd.dtacoffee.controller;
+
+import com.ttd.dtacoffee.App;
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Objects;
+import java.util.ResourceBundle;
+
+public class SplashScreenController implements Initializable {
+
+    @FXML
+    private AnchorPane splashScreen;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        splash();
+    }
+
+    private void splash(){
+        new Thread(){
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+
+                Platform.runLater(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/appView.fxml"));
+                            Scene scene = new Scene(fxmlLoader.load());
+                            Stage stage = new Stage();
+                            stage.setTitle("DTA Coffee Mangagement System");
+                            //Add icon
+                            Image icon256 = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/img/official-logo-256.png")));
+                            stage.getIcons().add(icon256);
+                            stage.setScene(scene);
+                            stage.setMaximized(true);
+                            stage.show();
+                            splashScreen.getScene().getWindow().hide();
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                });
+            }
+        }.start();
+    }
+}
